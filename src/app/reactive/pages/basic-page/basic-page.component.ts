6e3a146406ce91a2081ca59d83ac6d5a0426792c
mjlopez <maximilianojlopez@hotmail.com>
2025-05-07 16:01:35 -0300

    179

---
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basic-page',
+  imports: [],
+  templateUrl: './basic-page.component.html',
+
+})
+export class BasicPageComponent { }
---
+++ src/app/reactive/pages/reactive.routes.ts
@@ -0,0 +1,29 @@
+import { Routes } from "@angular/router";
+import { BasicPageComponent } from "./basic-page/basic-page.component";
+import { DynamicPageComponent } from "./dynamic-page/dynamic-page.component";
+import { SwitchesPageComponent } from "./switches-page/switches-page.component";
+
+
+export const authRoutes: Routes = [
+
+  {
+    path: '',
+    children: [
+      {
+        path:'basic',
+        title: 'Básicos',
+        component: BasicPageComponent,
+      },
+      {
+        path:'dynamic',
+        title: 'Dinámicos',
+        component: DynamicPageComponent,
+      },
+      {
+        path:'switches',
+        title: 'switches',
+        component: SwitchesPageComponent,
+      },
+    ]
+  },
+];
