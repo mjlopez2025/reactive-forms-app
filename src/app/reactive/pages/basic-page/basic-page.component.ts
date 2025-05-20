bb139af97cafc3d1763c0f89c3f22d276e528e5b
mjlopez <maximilianojlopez@hotmail.com>
2025-05-20 09:49:46 -0300

    185

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,6 +1,7 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
   selector: 'app-basic-page',
@@ -11,6 +12,7 @@ import { Form, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validat
 export class BasicPageComponent {
 
   private fb = inject(FormBuilder);
+  formUtils = FormUtils;
 
   myForm: FormGroup = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
@@ -26,30 +28,30 @@ export class BasicPageComponent {
   //   inStorage: new FormControl(0),
   // });
 
-  isValidField( fieldName: string) : boolean | null {
-    return (
-      this.myForm.controls[fieldName].errors &&
-      this.myForm.controls[fieldName].touched
-    );
-  }
+  // isValidField( fieldName: string) : boolean | null {
+  //   return (
+  //     this.myForm.controls[fieldName].errors &&
+  //     this.myForm.controls[fieldName].touched
+  //   );
+  // }
 
-  getFieldError( fieldName: string ): string | null {
-    if( !this.myForm.controls[ fieldName ]) return null;
+  // getFieldError( fieldName: string ): string | null {
+  //   if( !this.myForm.controls[ fieldName ]) return null;
 
-    const errors =this.myForm.controls[ fieldName ].errors ?? {};
+  //   const errors =this.myForm.controls[ fieldName ].errors ?? {};
 
-    for( const key of Object.keys(errors) ) {
-      switch( key ) {
-        case 'required':
-          return 'Este campo es requerido';
-        case 'minlength':
-          return `Minimo de ${errors['minlength'].requiredLength} caracteres.`;
-        case 'min':
-          return `Valor minimo de ${errors['min'].min}`;
-      }
-    }
-    return null;
-  }
+  //   for( const key of Object.keys(errors) ) {
+  //     switch( key ) {
+  //       case 'required':
+  //         return 'Este campo es requerido';
+  //       case 'minlength':
+  //         return `Minimo de ${errors['minlength'].requiredLength} caracteres.`;
+  //       case 'min':
+  //         return `Valor minimo de ${errors['min'].min}`;
+  //     }
+  //   }
+  //   return null;
+  // }
 
   onSave() {
     if( this.myForm.invalid){
---
+++ src/app/utils/form-utils.ts
@@ -0,0 +1,31 @@
+import { FormGroup } from "@angular/forms";
+
+export class FormUtils {
+
+  static isValidField( form: FormGroup, fieldName: string) : boolean | null {
+    return (
+      !! form.controls[fieldName].errors && form.controls[fieldName].touched
+    );
+  };
+
+  static getFieldError( form: FormGroup, fieldName: string ): string | null {
+    if( !form.controls[ fieldName ]) return null;
+
+    const errors = form.controls[ fieldName ].errors ?? {};
+
+    for( const key of Object.keys(errors) ) {
+      switch( key ) {
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Minimo de ${errors['minlength'].requiredLength} caracteres.`;
+        case 'min':
+          return `Valor minimo de ${errors['min'].min}`;
+      }
+    }
+    return null;
+  }
+
+
+
+}
