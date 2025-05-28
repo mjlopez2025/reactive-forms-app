2992dc7d61e4b28bbf83253ae3f334d806aafca9
mjlopez <maximilianojlopez@hotmail.com>
2025-05-28 14:40:54 -0300

    189

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,6 +1,6 @@
 import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
-import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { FormUtils } from '../../../utils/form-utils';
 
 @Component({
@@ -23,13 +23,27 @@ export class DynamicPageComponent {
     ),
   });
 
+  newFavorite = new FormControl('', Validators.required);
+
+
   get favoriteGames() {
     return this.myForm.get('favoriteGames') as FormArray;
   }
 
-  // isValidFieldInArray(formArray: FormArray, index: number) {
-  //   return (
-  //     formArray.controls[index].errors && formArray.controls[index].touched
-  //   );
-  // }
+  onAddFavorites() {
+    if( this.newFavorite.invalid) return;
+    const newGame = this.newFavorite.value;
+
+    this.favoriteGames.push(this.fb.control( newGame, Validators.required))
+
+    this.newFavorite.reset();
+  }
+
+  onDeleteFavorite( index: number) {
+    this.favoriteGames.removeAt(index);
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
  }
