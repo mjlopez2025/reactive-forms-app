ce43f6a95ddc1e017305458e5ca88812b10295d5
Maxi <mjlopez@undav.edu.ar>
2025-06-06 10:54:47 -0300

    203

--- src/app/country/pages/country-page/country-page.component.ts
+++ src/app/country/pages/country-page/country-page.component.ts
@@ -1,8 +1,29 @@
-import { Component } from '@angular/core';
+import { CountryService } from './../../services/country.service';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Country } from '../../interafces/country.interface';
 
 @Component({
   selector: 'app-country-page',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent { 
+  fb = inject(FormBuilder);
+  CountryService = inject(CountryService);
+
+  
+  regions = signal(this.CountryService.regions);
+  countriesByRegion = signal<Country[]>([]);
+  borders = signal<Country[]>([]);
+
+
+  myForm = this.fb.group({
+    region: ['', Validators.required],
+    country: ['', Validators.required],
+    border: ['', Validators.required],
+  });
+
+}
+ 
