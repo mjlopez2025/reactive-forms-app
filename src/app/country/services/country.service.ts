457fe53e4994c4b693707c014b772b1aaf18b831
Maxi <mjlopez@undav.edu.ar>
2025-06-06 10:34:44 -0300

    202

---
+++ src/app/country/services/country.service.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Country } from '../interafces/country.interface';
+
+@Injectable({providedIn: 'root'})
+export class CountryService {
+    
+    private baseurl = 'https://restcountries.com/v3.1';
+    private http = inject(HttpClient);
+    
+    private _regions = [
+        'Africa',
+        'Americas',
+        'Asia',
+        'Europe',
+        'Oceania'
+    ]
+
+    get regions(): string[] {
+        return [...this._regions];
+    }
+
+    getCountriesByRegion(region: string): Observable<Country[]> {
+        if (!region) return of([]);
+        console.log({region});
+
+        const url = `${this.baseurl}/region/${region}?fields=cca3,name,borders`;
+        return this.http.get<Country[]>(url);
+    }
+
+    getCountryByAlphaCode(alphaCode: string): Observable<Country> {
+    const url = `${this.baseurl}/alpha/${alphaCode}?fields=cca3,name,borders`;
+        return this.http.get<Country>(url);
+    }     
+
+    getCountryBordersCodes(borders: string[]){
+
+    }
+}
